fde22854c5b9e138eabaad74d082d3bee1fc5a55
jin-lee <pplaneoo@gmail.com>
2022-03-28 05:09:10 +0900

    step07: Math1

---
+++ src/step07/Step07_03.java
@@ -0,0 +1,33 @@
+// 1193. 분수찾기 (https://www.acmicpc.net/problem/1193)
+
+package step07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step07_03 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+
+        int level = -1;
+        while (x > 0) { x -= ++level; }
+
+        int row = 0;
+        int col = 0;
+
+        if (level % 2 == 0) {
+            row = level + x;
+            col = level - row + 1;
+        } else {
+            col = level + x;
+            row = level - col + 1;
+        }
+
+        System.out.println(row + "/" + col);
+    }
+}
