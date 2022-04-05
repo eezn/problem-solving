450306183d5625315f9fa369f56afc2efa7cf545
jin-lee <pplaneoo@gmail.com>
2022-04-05 12:58:50 +0900

    -

--- src/step01/Step01_01.java
+++
@@ -1,9 +0,0 @@
-// 2557. Hello World (https://www.acmicpc.net/problem/2557)
-
-package step01;
-
-public class Step01_01 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
