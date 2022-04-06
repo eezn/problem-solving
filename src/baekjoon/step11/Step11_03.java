cdaa07a30edb899c8b31c5502fa923832f26541b
jin-lee <pplaneoo@gmail.com>
2022-04-06 19:03:06 +0900

    step11: Counting Sort

---
+++ src/baekjoon/step11/Step11_03.java
@@ -0,0 +1,29 @@
+// 10989. 수 정렬하기 3 (https://www.acmicpc.net/problem/10989)
+
+package baekjoon.step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step11_03 {
+
+    final static int RANGE = 10000;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int len = Integer.parseInt(br.readLine());
+        int[] digit = new int[RANGE + 1];
+
+        for (int i = 0; i < len; ++i)
+            digit[Integer.parseInt(br.readLine())]++;
+
+        for (int i = 0; i < RANGE + 1; ++i)
+            for (int j = 0; j < digit[i]; ++j)
+                sb.append(i).append('\n');
+
+        System.out.print(sb);
+    }
+}
