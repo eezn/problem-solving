34ff2b00da34b2262216754a25f590045ce05a1b
jin-lee <pplaneoo@gmail.com>
2022-04-13 21:29:50 +0900

    step15: Greedy

---
+++ src/baekjoon/step15/Step15_05.java
@@ -0,0 +1,38 @@
+// S4 13305. 주유소 (https://www.acmicpc.net/problem/13305)
+
+package baekjoon.step15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Step15_05 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine()) - 1;
+        long[] distance = new long[n];
+        long[] oilPrice = new long[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i)
+            distance[i] = Long.parseLong(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i)
+            oilPrice[i] = Long.parseLong(st.nextToken());
+
+        long answer = 0;
+        long currPrice = oilPrice[0];
+        for (int i = 0; i < n; ++i) {
+            if (currPrice > oilPrice[i])
+                currPrice = oilPrice[i];
+            answer += currPrice * distance[i];
+        }
+
+        System.out.println(answer);
+    }
+}
