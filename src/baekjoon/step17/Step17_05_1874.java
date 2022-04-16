c5c3cd47f3eb7b045e511fe9b2578c3b91e08b4e
jin-lee <pplaneoo@gmail.com>
2022-04-16 15:53:32 +0900

    step17: Stack

--- src/baekjoon/step17/Step17_05_1874.java
+++ src/baekjoon/step17/Step17_05_1874.java
@@ -2,5 +2,51 @@
 
 package baekjoon.step17;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Step17_05_1874 {
+
+    static StringBuilder sb = new StringBuilder();
+    static Stack<Integer> stack = new Stack<>();
+
+    private static void push(int i) {
+        sb.append("+\n");
+        stack.push(i);
+    }
+
+    private static int pop() {
+        sb.append("-\n");
+        return stack.pop();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] target = new int[n];
+
+        for (int i = 0; i < n; ++i)
+            target[i] = Integer.parseInt(br.readLine());
+
+        int idx = 0;
+
+        for (int i = 1; i < n + 1; ++i) {
+            push(i);
+            while (!stack.isEmpty()) {
+                if (stack.peek() == target[idx]) {
+                    pop();
+                    idx++;
+                } else break;
+            }
+        }
+
+        if (!stack.isEmpty())
+            System.out.println("NO");
+        else
+            System.out.print(sb);
+    }
 }
