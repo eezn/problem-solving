4e1362bb41f81d2cf445c0b9d11d31db17dc5515
jin-lee <pplaneoo@gmail.com>
2022-05-15 22:59:08 +0900

    step11: Sort

---
+++ src/baekjoon/step11/Step11_05_1427.java
@@ -0,0 +1,23 @@
+// S5 1427. 소트인사이드 (https://www.acmicpc.net/problem/1427)
+
+package baekjoon.step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Step11_05_1427 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length - 1; i >= 0; i--)
+            sb.append(arr[i]);
+
+        System.out.println(sb);
+    }
+}
